c27dadb489c74af738a322aa467bbd041fb347f6
Vedts-lokanath <lokanath.vedts@gmail.com>
2026-08-19 15:11:23 +0530

    commit

--- src/component/training/requisition.jsx
+++ src/component/training/requisition.jsx
@@ -249,7 +249,6 @@ const Requisition = () => {
                                 )}
 
                                 {Number(item.initiatingOfficer) === Number(empId) &&
-                                    item.isAttend === "Y" &&
                                     !feedbackExists && (item.status === "CO" || item.status === "FA") && (
                                         <button
                                             className="btn btn-sm btn-secondary me-2"
